985486dc86df8817dde0517c7a9b65d14199d7d1
Sakshamsahu302 <sakshamsahu17@gmail.com>
2024-12-25 09:57:21 +0530

    Completed History of Python

---
+++ python/basics/1_History.py
@@ -0,0 +1,46 @@
+'''If we are talking about ABC in python context then it means that the programming language ABC is reffered which was a general purpose programming language developed in nethierlands amsterdam
+at the CWI(Centrum Wiskunde and Informatica) the gratest achievement of the ABC was to influence the design of python. python waas conceptualized in late 1980's when 
+ guido van rossum was working on a project at CWI called amoeba which was a distributed OS.'''
+
+'''The first realeased  version of python code was 0.9.0 at alt.sources on feburary 1991 '''
+'''This version already included exception halding,functions and core data types of the list dict,str , and others.'''
+
+'''the python version 1.0 was released in janurary 1994 it included new features which were functional programming tools lamda,map,filter and reduce.'''
+
+'''after 6.5 years on october 2000 python 2.0 was releaASED it included list comprehensions, a full garbage collector and it was supporting unicode.(Unicode is a universal character encoding standard that is maintained by the Unicode Consortium)'''
+
+'''python flourished for another 8 years in versions 2.x before the next major release of python 3.0(also known as python3000 and Py3K) python 3 is not backwards compatible with python 2.x'''
+
+'''the emphasis on python 3 was on the removal of duplicate programming construct and modules coming close to fulfilling the 13th law of the Zen of Python: "There should be one -- and preferably only one -- obvious way to do it."'''
+
+'''The Zen of Python
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one -- and preferably only one -- obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those'''
+
+
+'''Some changes in Python 3.0:
+Print is now a function
+Views and iterators instead of lists
+The rules for ordering comparisons have been simplified. E.g. a heterogeneous list cannot be sorted, because all the elements of a list must be comparable to each other.
+There is only one integer type left, i.e. int. long is int as well.
+The division of two integers returns a float instead of an integer. "//" can be used to have the "old" behaviour.
+Text Vs. Data Instead Of Unicode Vs. 8-bit'''
+
+
+
